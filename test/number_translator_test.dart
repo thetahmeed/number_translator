b2b2a3e39cc3c58f50f7abed9f40b38bda6de44b
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2024-04-16 18:09:43 +0600

    Update number_translator_test.dart

--- test/number_translator_test.dart
+++ test/number_translator_test.dart
@@ -2,9 +2,14 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:number_translator/number_translator.dart';
 
 void main() {
+  test('From English', () {
+    String translatedNumber =
+        NumberTranslator.translate('123', toLanguage: Language.bangla);
+    print(translatedNumber); // Output: '১২৩'
+  });
+
   test('To English', () {
-    NumberTranslator nt = NumberTranslator();
-    String translatedNumber = nt.toEn('১২৩');
+    String translatedNumber = NumberTranslator.translateToEnglish('১২৩');
     print(translatedNumber); // Output: '123'
   });
 }
